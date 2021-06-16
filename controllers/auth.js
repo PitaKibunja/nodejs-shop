e705b0caf4ee2bf2333ec2f66742f426fe78300c
dapintechnologies <dapintech@gmail.com>
2021-06-16 22:55:31 +0300

    working on reset-password form

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,7 +1,17 @@
 const bcrypt = require('bcryptjs');
+const crypto = require('crypto')
 
 const User = require('../models/user');
+const nodemailer = require('nodemailer')
 
+var transport = nodemailer.createTransport({
+  host: "smtp.mailtrap.io",
+  port: 2525,
+  auth: {
+    user: "602544fe38b0e6",
+    pass: "019b98ca8025d9"
+  }
+});
 exports.getLogin = (req, res, next) => {
   let message = req.flash('error')
   if (message.length > 0) {
@@ -95,7 +105,17 @@ exports.postSignup = (req, res, next) => {
         })
         .then(result => {
           res.redirect('/login');
-        });
+         return transport.sendMail({
+
+            from:'node@shop.com',
+            to: email,
+            subject: 'Signup success',
+            html:'<b>Signed up to Node shop successfully</b>'
+          })
+          
+        }).catch(err => {
+          console.log(err)
+        })
     })
     .catch(err => {
       console.log(err);
@@ -108,3 +128,76 @@ exports.postLogout = (req, res, next) => {
     res.redirect('/');
   });
 };
+
+exports.getReset = (req, res, next) => {
+  let message = req.flash('error')
+  if (message.length > 0) {
+    message=message[0]
+  } else {
+    message=null
+  }
+  res.render('auth/reset', {
+    path: '/reset',
+    pageTitle: ' Reset Password..',
+    errorMessage: message
+  });
+
+}
+
+exports.postReset = (req, res, next) => {
+  crypto.randomBytes(32, (err, buffer) => {
+    if (err) {
+      console.log(err)
+      return res.redirect('reset')    
+    }
+    const token = buffer.toString('hex')
+    User.findOne({ email: req.body.email })
+      .then(user => {
+        if (!user) {
+          req.flash('error', 'No account with that email found')
+          return res.redirect('/reset')
+        }
+        user.resetToken = token
+        user.resetTokenExpiration = Date.now() + 3600000
+        return user.save()
+
+      }).then(result => {
+        //send the token to the email, since
+        //we assume that the token was saved in the db
+        res.redirect('/')
+
+        transport.sendMail({
+          from:'node@shop.com',
+          to: req.body.email,
+          subject: 'Password Reset',
+          html: `
+          <p>You requested a password reset</p>
+          <p>Click this <a href="http://localhost:3000/reset/${token}">link</a> to set a new password
+
+          `
+        })
+      })
+      .catch(err => {
+      
+        console.log(err)
+        
+    })
+    
+  })
+}
+
+exports.getNewPassword=(req,res,next)=>{
+  let message = req.flash('error')
+  if (message.length > 0) {
+    message=message[0]
+  } else {
+    message=null
+  }
+  res.render('auth/new-password', {
+    
+    path:'/new-password',
+    pageTitle:'New Password',
+    errorMessage: message
+    
+  })
+}
