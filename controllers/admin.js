9d18c63ba8c1db458ec6fbc128bcd6037834c6cc
dapintechnologies <dapintech@gmail.com>
2021-07-02 06:26:34 +0300

    almost done with delete lec

--- controllers/admin.js
+++ controllers/admin.js
@@ -1,6 +1,7 @@
 const mongoose=require('mongoose')
 const { validationResult } = require('express-validator')
 const Product = require('../models/product');
+const fileHelper = require('../util/file')
 
 exports.getAddProduct = (req, res, next) => {
   res.render('admin/edit-product', {
@@ -154,6 +155,7 @@ exports.postEditProduct = (req, res, next) => {
       product.price = updatedPrice;
       product.description = updatedDesc;
       if (image) {
+        fileHelper.deleteFile(product.imageUrl)
         product.imageUrl = image.path;
       }
      
@@ -188,6 +190,12 @@ exports.getProducts = (req, res, next) => {
 
 exports.postDeleteProduct = (req, res, next) => {
   const prodId = req.body.productId;
+  Product.findById(prodId).then(product => {
+    if (!product) {
+      return next(new Error('Product Not Found'))
+    }
+    fileHelper.deleteFile(product.imageUrl)
+  }).catch(err=>next(err))
   Product.deleteOne({ _id: prodId, userId: req.user._id })
     .then(() => {
       console.log('DESTROYED PRODUCT');
--- controllers/shop.js
+++ controllers/shop.js
@@ -1,5 +1,7 @@
 const fs = require('fs');
 const path = require('path');
+const PDFDocument=require('pdfkit')
+
 
 const Product = require('../models/product');
 const Order = require('../models/order');
@@ -160,8 +162,44 @@ exports.getInvoice = (req, res, next) => {
       if (order.user.userId.toString() !== req.user._id.toString()) {
         return next(new Error('Unauthorized'));
       }
-      // const invoiceName = 'invoice-' + orderId + '.pdf';
-      // const invoicePath = path.join('data', 'invoices', invoiceName);
+      const invoiceName = 'invoice-' + orderId + '.pdf';
+      const invoicePath = path.join('data', 'invoices', invoiceName);
+      
+      const pdfDoc = new PDFDocument()
+      res.setHeader('Content-Type', 'application/pdf');
+      res.setHeader(
+        'Content-Disposition',
+        'inline; filename="' + invoiceName + '"'
+      );
+      pdfDoc.pipe(fs.createWriteStream(invoicePath))
+      pdfDoc.pipe(res)
+      
+      // pdfDoc.text('Hello World')
+      pdfDoc.fontSize(26).text('Invoice', {
+        underline:true
+      })
+      pdfDoc.text('-------------------------')
+      let totalPrice=0
+      order.products.forEach(prod => {
+
+        totalPrice += prod.quantity * prod.product.price
+        
+        pdfDoc.fontSize(14).text(
+          prod.product.title
+          + '-' +
+          prod.quantity
+          + '  X' +
+          '  Kshs.' +
+          prod.product.price
+        )
+      })
+      pdfDoc.text('------------')
+      pdfDoc.fontSize(20).text('Total Price: Kshs. '+totalPrice)
+      pdfDoc.end()
+      
+      
+      
+      
       // fs.readFile(invoicePath, (err, data) => {
       //   if (err) {
       //     return next(err);
@@ -173,13 +211,9 @@ exports.getInvoice = (req, res, next) => {
       //   );
       //   res.send(data);
       // });
-      const file = fs.createReadStream(invoicePath);
-      res.setHeader('Content-Type', 'application/pdf');
-      res.setHeader(
-        'Content-Disposition',
-        'inline; filename="' + invoiceName + '"'
-      );
-      file.pipe(res);
+      // const file = fs.createReadStream(invoicePath);
+      
+      // file.pipe(res);
     })
     .catch(err => next(err));
 };
