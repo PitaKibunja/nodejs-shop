6acaf464f49efc00b5dda5f21a55038fa7466a2f
dapintechnologies <dapintech@gmail.com>
2021-07-03 20:12:52 +0300

    Stripe has an error

--- controllers/shop.js
+++ controllers/shop.js
@@ -156,12 +156,12 @@ exports.getCheckout = (req, res, next) => {
             name: p.productId.title,
             description: p.productId.description,
             amount: p.productId.price * 100,
-            currency='usd',
+            currency:'usd',
             quantity:p.quantity
           }
         }),
         success_url: req.protocol + '://' + req.get('host')+'/checkout/success', //=>http://localhost:3000
-        cancel_url:req.protocol + '://' + req.get('host')+'/checkout/success'
+        cancel_url:req.protocol + '://' + req.get('host')+'/checkout/cancel'
       })
 
 
@@ -180,6 +180,35 @@ exports.getCheckout = (req, res, next) => {
     return next(error);
   });
 }
+exports.getCheckoutSuccess = (req, res, next) => {
+  req.user
+    .populate('cart.items.productId')
+    .execPopulate()
+    .then(user => {
+      const products = user.cart.items.map(i => {
+        return { quantity: i.quantity, product: { ...i.productId._doc } };
+      });
+      const order = new Order({
+        user: {
+          email: req.user.email,
+          userId: req.user
+        },
+        products: products
+      });
+      return order.save();
+    })
+    .then(result => {
+      return req.user.clearCart();
+    })
+    .then(() => {
+      res.redirect('/orders');
+    })
+    .catch(err => {
+      const error = new Error(err);
+      error.httpStatusCode = 500;
+      return next(error);
+    });
+};
 
 exports.postOrder = (req, res, next) => {
   req.user
